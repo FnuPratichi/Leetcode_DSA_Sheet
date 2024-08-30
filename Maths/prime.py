2098cbe5cc02cd7bb0de857f090d0cd9e7d410f6
Fnu Pratichi <pratichi3@gmail.com>
2024-08-30 18:02:04 -0400

    newchnages

---
+++ Maths/prime.py
@@ -0,0 +1,11 @@
+names = input()
+times = int(input())
+for _ in range(times):
+    print(names)
+
+
+
+names = input()
+times = int(input())
+for i in range(times):
+    print(names)
--- Recursion/reverse.py
+++ Recursion/reverse.py
@@ -26,3 +26,4 @@ print(rever(number,0,len(str(number))-1))
 
 
 
+
