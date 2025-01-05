8befabbdcd7610082b9f5f84066824e7a2845654
FNU Pratichi <fnupratichi@FNUs-Laptop.local>
2025-01-05 16:37:22 -0500

    new string codes

---
+++ check_alphanumericstring.py
@@ -0,0 +1,16 @@
+def fun(s):
+    alnum = False
+
+    for char in s:
+        if char.isalnum():
+            alnum = True
+            break
+
+
+    if alnum == True:
+        print("True")
+    else:
+        print("False")
+
+        
+fun('anku')
