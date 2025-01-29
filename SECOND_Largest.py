47d00eadfa06cf237649477e707e312b86fb9c57
Fnu Pratichi <pratichi3@gmail.com>
2025-01-28 20:03:05 -0500

    new codes

---
+++ SECOND_Largest.py
@@ -0,0 +1,14 @@
+num = [11,20,10,0,15]
+
+largest_num = num[0]
+second_largest = -1
+
+for i in range(0,len(num)):
+    if num[i] > largest_num:
+        second_largest = largest_num
+        largest_num=num[i]
+print('largest number is' , largest_num)
+
+if num[i] > second_largest and num[i]!=largest_num:
+    second_largest = num[i]
+print('second largest no is', second_largest)
