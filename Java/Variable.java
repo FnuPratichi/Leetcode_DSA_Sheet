95cac63a8681512c2d13b5b381a594ed2a9b21a5
Fnu Pratichi <pratichi3@gmail.com>
2025-04-09 16:46:08 -0400

    initial java code

---
+++ Java/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    public static void main(String args[]){
+        String name = "Pratichi";
+        int a = 10;
+        int b = 20;
+        float c = 10f;
+        float d = 12f;
+        boolean flag = true;
+        char my_char = 'A';
+        System.out.println(name);
+        System.out.println(a+b);
+        System.out.println(c+d);
+        System.out.println(flag);
+        System.out.println(my_char);
+
+        int myNum = 15;
+        myNum = 20;  
+        System.out.println(myNum);   // overwrite the previous value:
+
+
+    }
+    
+
+}
---
+++ Java/practice1.java
@@ -0,0 +1,12 @@
+public class practice1 {
+    public static void main(String args[]){
+        System.out.println("here Hi");
+        System.out.println("Hello");
+        System.out.print("here Hi");
+        System.out.print(" Hi");
+        System.out.println(2 * 5);
+        System.out.println(2+3);
+    }
+}
+
+/*  Comments for java program */
