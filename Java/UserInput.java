2507edaa1d471e601475ea4e886603e56253ba15
Fnu Pratichi <pratichi3@gmail.com>
2025-05-20 16:14:42 -0400

    user input

--- Java/UserInput.java
+++ Java/UserInput.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class UserInput {
     public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+        int age = scanner.nextInt();
+        System.out.println("Your age is "+ age + "years old");
+
         
     }
     
